456dce8b7bcbbdc0dc8dc4c4af293cf6a29ba27f
Melwyn Titus <melwyntj232411@ascn.sies.edu.in>
2025-01-20 23:15:48 +0530

    frontend updates and a frontend branch created

--- ocr-frontend/app/page.js
+++ ocr-frontend/app/page.js
@@ -1,5 +1,4 @@
 'use client'; // Mark this as a Client Component
-
 import { useState } from 'react';
 import Header from './components/Header';
 import UploadComponent from './components/UploadComponent';
@@ -12,7 +11,7 @@ export default function Home() {
     <div className='w-full h-full pb-4'>
       <Header />
       <div className='sm:flex sm:flex-col sm:justify-center sm:h-full xs:flex xs:flex-col xs:justify-center xs:h-full md:flex md:flex-col md:justify-center md:h-full lg:flex lg:flex-col lg:justify-center lg:h-full'>
-        <div className='flex flex-col items-center justify-center lg:mt-16 md:mt-16 2xl:mt-0 gap-3 px-4 md:px-0'>
+        <div className='flex flex-col items-center justify-center lg:mt-40 md:mt-16 2xl:mt-0 gap-3 px-4 md:px-0'>
           <h1 className='font-poppins text-2xl sm:text-3xl md:text-4xl font-bold text-[#1F2937] mt-4 text-center'>
             <span className='special-text'>Snap</span> a Pic, Get Text <span className='special-text'>Quick</span>
           </h1>
