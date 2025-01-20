d8212b2100f40a8d2a0689a25d2acb9a3612c255
Melwyn Titus <melwyntj232411@ascn.sies.edu.in>
2025-01-20 22:17:15 +0530

    next.config.mjs update

--- ocr-frontend/next.config.mjs
+++ ocr-frontend/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export', // Required for static exporting
+    trailingSlash: true, // Ensures all routes have trailing slashes (important for static hosting)
+    images: {
+        unoptimized: true, // Needed if you're using the Next.js <Image> component with static exports
+    },
+};
 
 export default nextConfig;
