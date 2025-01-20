53bb105dbb5ab2979805f8b8bd48264ac3b02f87
Melwyn Titus <melwyntj232411@ascn.sies.edu.in>
2025-01-20 22:24:46 +0530

    next.config.mjs rollback

--- ocr-frontend/next.config.mjs
+++ ocr-frontend/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', // Required for static exporting
-    trailingSlash: true, // Ensures all routes have trailing slashes (important for static hosting)
-    images: {
-        unoptimized: true, // Needed if you're using the Next.js <Image> component with static exports
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
